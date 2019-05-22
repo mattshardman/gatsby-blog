0301ada9db88206e834e519b353efb2ba020e2f7
mattshardman <mattshardman@gmail.com>
2019-05-22 09:02:37 +0100

    feature(style terminal component)

--- src/components/mac/Terminal.jsx
+++ src/components/mac/Terminal.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    height: 50%;
-    width: 50%;
-    background: black;
+    height: 75%;
+    width: 75%;
+    background: #000;
+    border-radius: 5px;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.2);
+`;
+
+const TopBar = styled.div`
+    width: 100%;
+    height: 10%;
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+`;
+
+const Button = styled.div`
+    height: 5px;
+    width: 5px;
+    border-radius: 50%;
+    margin-left: 5px;
+    background: ${({ color }) => color};
 `;
 
 function Terminal() {
+  const [text, setText] = useState();
   return (
-    <Container />
+    <Container>
+      <TopBar>
+        { ['red', 'orange', 'green'].map(btn => <Button color={btn} />)}
+      </TopBar>
+    </Container>
   );
 }
 
