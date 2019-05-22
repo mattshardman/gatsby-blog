6e758bd8f18a1101e2e6d165d452735ea0ce98fc
mattshardman <mattshardman@gmail.com>
2019-05-22 11:00:24 +0100

    feature(add message bubbles to mobile home)

--- src/components/iphone/App.jsx
+++ src/components/iphone/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import MessageBubble from './MessageBubble';
 
 const Container = styled.div`
+    position: relative;
     height: 100%;
     width: 100%;
     background: #fff;
@@ -9,6 +12,7 @@ const Container = styled.div`
     animation: move 300ms 1 forwards;
     animation-delay: 1s;
     transform: translateY(100%);
+    font-size: 12px;
 
     @keyframes move {
         from {
@@ -21,11 +25,38 @@ const Container = styled.div`
     }
 `;
 
+const Heading = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 0 0px 0;
+    border-bottom: 1px solid #eaeaea;
+`;
+
+const Text = styled.div`
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+`;
+
 function IPhoneApp() {
   return (
     <Container>
-        
-        </Container>
+      <Heading>
+        <Avatar />
+        <small style={{ padding: 5 }}>Matt</small>
+      </Heading>
+      <Text>
+        <MessageBubble>
+          Hi I&apos;m Matt.
+        </MessageBubble>
+        <MessageBubble>
+          I am a full-stack javascript developer from the UK.
+        </MessageBubble>
+      </Text>
+    </Container>
   );
 }
 
---
+++ src/components/iphone/MessageBubble.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import PT from 'prop-types';
+
+const MessageBubble = ({ children, right = false }) => (
+  <div className="wrapper">
+    <div className="call-out">
+      <div className={right ? 'cut-out-right' : 'cut-out'}>
+        <div className={right ? 'circle-right' : 'circle'} />
+      </div>
+      <div className="bubble">
+        <div className="text">
+          {children}
+        </div>
+      </div>
+    </div>
+    <style jsx>{`
+        .wrapper {
+          box-sizing: border-box;
+          position: relative;
+          max-width: 100%;
+          margin: 0 10px;
+          padding-top: 10px;
+          animation-name: appear;
+          animation-duration: 200ms;
+          animation-iteration-count: 1;
+        }
+
+        @keyframes appear {
+          from {
+            transform: translateX(${right ? '120%' : '-120%'});
+          }
+
+          to {
+            transform: translateX(0);
+          }
+        }
+
+        .call-out {
+          box-sizing: border-box;
+          display: flex;
+          justify-content: ${right ? 'flex-end' : 'flex-start'} ;
+        }
+
+        .bubble {
+          box-sizing: border-box;
+          border-radius: ${right ? '20px 0px 20px 20px' : '0px 20px 20px 20px'};
+          border: 1px solid #eaeaea;
+          background: ${right ? '#eaeaea' : '#fff'}
+        }
+
+        .cut-out {
+          margin-top: 1px;
+          top: 0;
+          position: absolute;
+          height: 12px;
+          width: 20px;
+          border-left: 1px solid #eaeaea;
+          overflow: hidden;
+          background: #fff;
+        }
+
+        .cut-out-right {
+          margin-top: 1px;
+          top: 0;
+          right: 0;
+          position: absolute;
+          height: 12px;
+          width: 20px;
+          border-right: 1px solid #eaeaea;
+          overflow: hidden;
+          background: #eaeaea;
+        }
+
+        .circle {
+          position: absolute;
+          bottom: 2px;
+          left: -1px;
+          width: 42px;
+          height: 22px;
+          border-radius: 50%;
+          border: 1px #eaeaea solid;
+          background: #F7F9FC;
+        }
+
+        .circle-right {
+          position: absolute;
+          bottom: 2px;
+          right: -1px;
+          width: 42px;
+          height: 22px;
+          border-radius: 50%;
+          background: #F7F9FC;
+          border: 1px #eaeaea solid;
+        }
+
+        .text {
+          box-sizing: border-box;
+          color: #484848;
+          padding: 10px 0px;
+          margin: 0px 15px;
+          display: flex;
+          align-items: center;
+        }
+      `}
+
+    </style>
+  </div>
+);
+
+MessageBubble.propTypes = {
+  children: PT.string.isRequired,
+  right: PT.bool.isRequired,
+};
+
+export default MessageBubble;
