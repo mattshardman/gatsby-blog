7a7253e630f0702bb419bc52285d589bf8be1c04
mattshardman <mattshardman@gmail.com>
2019-05-22 12:57:17 +0100

    feature(add go api for contact form)

---
+++ api/send/index.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Message struct {
+	Name string `json:"name"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	var msg Message
+	err = json.Unmarshal(body, &msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	output, err := json.Marshal(msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.Write(output)
+}
