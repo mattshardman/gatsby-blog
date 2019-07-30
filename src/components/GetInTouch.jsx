bf334c62a2ac897bd8edafaca0347407a8d550be
mattshardman <mattshardman@gmail.com>
2019-07-30 15:40:18 +0100

    create get in touch component

---
+++ src/components/GetInTouch.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import MainButton from './MainButton';
+
+const Container = styled.section`
+    width: 100%;
+    height: 300px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px #eaeaea solid;
+    border-bottom: 1px #eaeaea solid;
+    padding: 0 25%;
+    /* background: #ff0078; */
+    background: #fafbfc;
+
+    h1 {
+        margin: 0;
+        font-size: 40px;
+        color: #000;
+    }
+
+    @media(max-width: 600px) {
+      padding: 80px 5%;
+      flex-direction: column;
+    }
+`;
+
+const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
+
+function GetInTouch() {
+  return (
+    <Container>
+      <Text>
+        <h1>Like what you see?</h1>
+      </Text>
+      <Link to="/contact" style={{ width: '200px' }}>
+        <MainButton
+          text="Get in touch"
+          color="#fff"
+        // background="#24292e"
+          width="200px"
+          background="#ff0078"
+        />
+      </Link>
+    </Container>
+  );
+}
+
+export default GetInTouch;
