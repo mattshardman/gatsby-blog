63816bd799697d40a2947545b3de5d0c615fc3df
mattshardman <mattshardman@gmail.com>
2019-09-28 12:58:29 +0100

    add mobile width option

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -40,7 +40,7 @@ const Button = styled.button`
 
   @media (max-width: 600px) {
     margin: 10px 0;
-    width: 100%;
+    width: ${({ mobWidth }) => mobWidth || '100%'};
   }
 
   @keyframes fade {
